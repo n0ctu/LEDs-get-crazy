c0fcab62c73b6c41d5204cda1607547ce7407e2c
n0c <n0ctu@proton.me>
2024-01-04 00:35:14 +0100

    Updated setup/requirements

--- setup-systemd.sh
+++ setup-systemd.sh
@@ -2,14 +2,6 @@
 
 PROJECT_DIR=$(realpath $(dirname "$0"))
 
-if [ "$EUID" -ne 0 ]; then
-    # Invoke the script again with sudo
-    sudo bash "$0" "$@"
-    exit $?
-fi
-echo "Setup is running as root user."
-echo
-
 # Consent
 cat << "EOF"
  _     _____ ____                   _                              
@@ -32,10 +24,13 @@ then
     exit
 fi
 
+#sudo apt update
+#sudo apt -y upgrade
+
 echo "Check if python 3 is installed..."
 if ! command -v python3 &> /dev/null
 then
-    apt install python3
+    sudo apt -y install python3
     if ! command -v python3 &> /dev/null
     then
         echo "ERROR: Python 3 could not be installed"
@@ -43,41 +38,34 @@ then
     fi
 fi
 
-echo "Check if venv is installed..."
-if ! command -v python3 -m venv &> /dev/null
-then
-    pip install virtualenv
-    if ! command -v python3 -m venv &> /dev/null
-    then
-        echo "ERROR: Virtualenv could not be installed"
-        exit
-    fi
-fi
+sudo apt -y install python3-pip
+sudo apt -y install python3-venv
 
 # Create a new user 'ledsgc' if it doesn't exist
 if ! id "ledsgc" &>/dev/null; then
-    useradd -r -M -s /bin/false ledsgc
+    sudo useradd --system --shell /bin/false ledsgc
 fi
 
 # Add the user to the gpio group
-adduser ledsgc gpio
+sudo adduser ledsgc gpio
 
 # Give the user access rights to the folder
 ledsgc_group=$(id -gn ledsgc)
-chgrp -R "$ledsgc_group" "$PROJECT_DIR"
-chmod -R 770 "$PROJECT_DIR"
+sudo chgrp -R "$ledsgc_group" "$PROJECT_DIR"
+sudo chmod -R 770 "$PROJECT_DIR"
 
 # Create a Python virtual environment
-sudo -u ledsgc bash -c 'cd '"$PROJECT_DIR"' && python3 -m venv venv'
+cd $PROJECT_DIR
+python -m venv venv
 # Install Python dependencies
-sudo -u ledsgc bash -c 'cd '"$PROJECT_DIR"' && source venv/bin/activate && pip install -r requirements.txt'
+venv/bin/pip install -r requirements.txt
 
-find "$PROJECT_DIR" -type d -exec chmod 750 {} +
-find "$PROJECT_DIR" -type f -exec chmod 640 {} +
+sudo find "$PROJECT_DIR" -type d -exec sudo chmod 750 {} +
+sudo find "$PROJECT_DIR" -type f -exec sudo chmod 640 {} +
 
 # Make the scripts executable again
-chmod +x "$PROJECT_DIR"/setup-systemd.sh
-chmod +x "$PROJECT_DIR"/start-interface.sh
+sudo chmod +x "$PROJECT_DIR"/setup-systemd.sh
+sudo chmod +x "$PROJECT_DIR"/start-interface.sh
 
 # Define systemd service files for the interface and web components
 INTERFACE_SERVICE="[Unit]
@@ -99,11 +87,8 @@ WantedBy=multi-user.target"
 echo "$INTERFACE_SERVICE" | sudo tee /etc/systemd/system/ledsgc.service &> /dev/null
 
 # Reload systemd to recognize new services
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 # Enable the services to start on boot
-systemctl enable ledsgc.service
+sudo systemctl enable ledsgc.service
 echo "Systemd service 'ledsgc.service' has been set up and enabled."
-
-systemctl start ledsgc.service
-echo "Systemd service 'ledsgc.service' has been started."
