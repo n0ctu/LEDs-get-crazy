9fc7fa17cff32623c4b42ffac58ba8fd9d60c278
n0c <n0ctu@proton.me>
2024-01-05 09:20:01 +0100

    Platform support check added

--- interface/canvas.py
+++ interface/canvas.py
@@ -1,43 +1,48 @@
-import board
-import neopixel_write, digitalio
-
-# Check if the current platform is supported
-try:
-    pass
-except:
-    print("WARNING: The UDP interface requires a Raspberry Pi or similar device with GPIO pins. Development/Preview mode only (no board/no neopixel).")
-
 class Section:
     def __init__(self, pin, section_width, section_height, brightness=1.0, pixel_order='GRB'):
-        self.pin = self._init_pin(pin)
+        self._handle_imports()
+        if self.supported:
+            self.pin = self._init_pin(pin)
         self.section_width = section_width
         self.section_height = section_height
         self.num_pixels = section_width * section_height
         self.brightness = brightness
         self.pixel_order = pixel_order
     
+    def _handle_imports(self):
+        # Check if the current platform is supported
+        try:
+            self.board = __import__('board')
+            self.neopixel_write = __import__('neopixel_write')
+            self.digitalio = __import__('digitalio')
+            self.supported = True
+        except:
+            self.supported = False
+            print("WARNING: The UDP interface requires a Raspberry Pi or similar device with GPIO pins. Development/Preview mode only (no board/no neopixel).")
+
     def _init_pin(self, pin):
-        return digitalio.DigitalInOut(getattr(board, pin))
+        return self.digitalio.DigitalInOut(getattr(self.board, pin))
 
     def write(self, byte_array):
-        '''
-        To-Do:
-         Restructure the data to match the layout of the section
-         Calculate the brightness
-         Change color order if GRB
-         Reverse the whole data if true
-         Rotate the whole data if true
-        '''
+        if self.supported:
+            '''
+            To-Do:
+            Restructure the data to match the layout of the section
+            Calculate the brightness
+            Change color order if GRB
+            Reverse the whole data if true
+            Rotate the whole data if true
+            '''
 
-        # Send the data to the LED matrix
-        neopixel_write.neopixel_write(self.pin, byte_array)
+            # Send the data to the LED matrix
+            self.neopixel_write.neopixel_write(self.pin, byte_array)
 
 
 class Canvas:
     def __init__(self, config):
         self.config = config
         self.sections = self.init_sections()
-        
+
     def init_sections(self):
         sections = []
         # Initialize each section as an object
